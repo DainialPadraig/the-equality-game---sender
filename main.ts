f3343839b41fbe8484641049d8b20b826713373c
Dan Stormont <daniel.stormont@gmail.com>
2022-02-09 17:19:35 -0700

    Add visual indication that message is being sent.

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(randint(0, 3))
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+    basic.clearScreen()
 })
 radio.setGroup(1)
